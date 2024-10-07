cf232cb545e72be7aae905fbe102593536bb774f
ShivaniCoder <164142883+shivanigupta02501@users.noreply.github.com>
2024-10-07 12:47:58 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,7 +1,27 @@
 const decodeTheRing = function (s, p) {
-
-    // write your code here
-
+    const s = message.length;
+    const p = pattern.length;
+    const dp = Array(m + 1).fill(false).map(() => Array(p + 1).fill(false));
+    dp[0][0] = true; 
+    for (let j = 1; j <= p; j++) {
+        if (pattern[j - 1] === '*') {
+            dp[0][j] = dp[0][j - 1]; 
+        }
+    }
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= p; j++) {
+            if (pattern[j - 1] === message[i - 1] || pattern[j - 1] === '?') {
+                dp[i][j] = dp[i - 1][j - 1]; 
+            } else if (pattern[j - 1] === '*') {
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1]; 
+            }
+        }
+    }
+    return dp[m][p];
+}
+console.log(isMatch("aa", "a"));       
+console.log(isMatch("aa", "*"));       
+console.log(isMatch("cb", "?a"));      
+console.log(isMatch("adceb", "*a*b"));  
+console.log(isMatch("acdcb", "a*c?b")); 
   };
-  
-  module.exports = decodeTheRing;
